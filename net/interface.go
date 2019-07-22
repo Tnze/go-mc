c78d8369eb1362c2e8703cf8ca1d7a1319e4c8eb
Tnze <cjd001113@outlook.com>
2019-07-22 12:51:54 +0800

    add Reader and Writer interface of go-mc/net

---
+++ net/interface.go
@@ -0,0 +1,16 @@
+package net
+
+import pk "github.com/Tnze/go-mc/net/packet"
+
+type Writer interface {
+	WritePacket(p pk.Packet) error
+}
+
+type Reader interface {
+	ReadPacket() (pk.Packet, error)
+}
+
+type ReaderWriter interface {
+	Reader
+	Writer
+}
