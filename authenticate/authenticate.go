214816cbe7ed669c6e647b897842847307133ba9
MscBaiMeow <38121125+MscBaiMeow@users.noreply.github.com>
2019-08-01 12:35:02 +0800

    外置登陆支持

--- authenticate/authenticate.go
+++ authenticate/authenticate.go
@@ -25,11 +25,10 @@ type Payload struct {
 	RequestUser bool   `json:"requestUser"`
 }
 
-// Authenticate authenticates a user using their password.
-func Authenticate(user, passwd, AuthServer string) (respData Response, err error) {
-	if AuthServer == "" {
-		AuthServer = "https://authserver.mojang.com/authenticate"
-	}
+// Allow auth with thirdparty auth server.
+func AuthWithThirdPartyServer(user, passwd, AuthServer string) (respData Response, err error) {
+
+	AuthServer += "/authserver/authenticate"
 	j, err := json.Marshal(Payload{
 		Agent: Agent{
 			Name:    "Minecraft",
@@ -56,6 +55,60 @@ func Authenticate(user, passwd, AuthServer string) (respData Response, err error
 	}
 	PostRequest.Header.Set("User-Agent", "go-mc")
 	PostRequest.Header.Set("Connection", "keep-alive")
+	PostRequest.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(PostRequest)
+	if err != nil {
+		err = fmt.Errorf("post authenticate fail: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		err = fmt.Errorf("read authenticate resp fail: %v", err)
+		return
+	}
+	err = json.Unmarshal(body, &respData)
+	if err != nil {
+		err = fmt.Errorf("unmarshal json data fail: %v", err)
+		return
+	}
+	if respData.Error != "" {
+		err = fmt.Errorf("authenticate fail: {error: %q, errorMessage: %q, cause: %q}",
+			respData.Error, respData.ErrorMessage, respData.Cause)
+		return
+	}
+	return
+}
+
+// Authenticate authenticates a user using their password.
+func Authenticate(user, passwd string) (respData Response, err error) {
+	j, err := json.Marshal(Payload{
+		Agent: Agent{
+			Name:    "Minecraft",
+			Version: 1,
+		},
+		UserName:    user,
+		Password:    passwd,
+		ClientToken: "gomcbotauther",
+		RequestUser: true,
+	})
+	// fmt.Println(string(j))
+	if err != nil {
+		err = fmt.Errorf("encoding json fail: %v", err)
+		return
+	}
+
+	//Post
+	client := http.Client{}
+	PostRequest, err := http.NewRequest(http.MethodPost, "https://authserver.mojang.com/authenticate",
+		bytes.NewReader(j))
+	if err != nil {
+		err = fmt.Errorf("make request error: %v", err)
+		return
+	}
+	PostRequest.Header.Set("User-Agent", "go-mc")
+	PostRequest.Header.Set("Connection", "keep-alive")
+	PostRequest.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(PostRequest)
 	if err != nil {
 		err = fmt.Errorf("post authenticate fail: %v", err)
