0e959dfbc0009925611c26cab0eca4bde4462870
Tnze <cjd001113@outlook.com>
2021-01-09 13:25:51 +0800

    go generate ./... && go fmt ./...

--- data/lang/tl-ph/tl_ph.go
+++ data/lang/tl-ph/tl_ph.go
@@ -5,4 +5,4 @@ import "github.com/Tnze/go-mc/chat"
 
 func init() { chat.SetLanguage(Map) }
 
-var Map = map[string]string{}
+var Map = map[string]string{"block.minecraft.sand": "Buhangin", "block.minecraft.water": "Agwa", "createWorld.customize.custom.preset.drought": "Baisi", "createWorld.customize.custom.preset.waterWorld": "Daigdig na Agwa", "createWorld.customize.custom.useCaves": "Mga Kuweba", "createWorld.customize.custom.useVillages": "Mga Balangay", "gui.no": "Hindi", "gui.yes": "Oo", "menu.disconnect": "Magkalag", "options.chat.width": "Kaluwangan", "options.modelPart.hat": "Sambalilo", "selectWorld.delete": "Kaltasin", "selectWorld.deleteButton": "Kaltasin", "selectWorld.edit": "Bumago", "selectWorld.empty": "basio", "selectWorld.world": "Daigdig"}
